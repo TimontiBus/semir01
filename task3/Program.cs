bd747e3980165b7983a99eada3e76f40d626ba6e
Timofey Busarev <busarev05@inbox.ru>
2024-01-06 20:22:48 +0300

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int num = 17;
+
+if (num >= 10 && num <= 99)
+{
+	int first = num % 10;
+	int second = num / 10;
+	if (first > second)
+	{
+		Console.Write(first);
+	}
+	else if (second > first)
+	{
+		Console.Write(second);
+	}
+	else
+	{
+		Console.Write("Они равны");
+	}
+}
+else
+{
+	Console.Write("Выходит за пределы отрезка");
+}
+
+
